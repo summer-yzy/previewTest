cc9cd921223d9784c8e5274583fba438ab82b26b
阳志勇 <yang_software@163.com>
2024-06-28 17:04:19 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/previewTest/',
   publicDir: 'public',
-  build:{
-    outDir: 'docs',
+  server:{
+    host: true
   }
 })
